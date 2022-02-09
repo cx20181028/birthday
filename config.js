9ac7d35b00d6c97fd1bf4900c04e48ab1b152edd
cx20181028 <50649868+cx20181028@users.noreply.github.com>
2022-02-09 20:48:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -27,17 +27,17 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-       // "心爱的小可爱": "./imgs/xiaokeai.png",
-         "今天是你的生日": "./imgs/birthday.jpg",
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+       //  "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "来个蛋糕",
-        light_candle: "蜡烛点亮",
+        bannar_coming: "横幅来啦",
+        balloons_flying: "好像少点东西哦",
+        cake_fadein: "蛋糕来啦",
+        light_candle: "蜡烛点亮啦",
         wish_message: "祝你生日快乐",
         story: "A MESSAGE FOR YOU",
     }
